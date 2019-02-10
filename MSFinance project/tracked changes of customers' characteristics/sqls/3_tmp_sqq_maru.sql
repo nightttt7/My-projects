c5667d840eebab6edad2b99ea9ff810fed5a06fd
shiqingqi <hym601@163.com>
2019-02-11 00:21:47 +0100

    Update 3_tmp_sqq_maru.sql

--- MSFinance project/tracked changes of customers' characteristics/sqls/3_tmp_sqq_maru.sql
+++ MSFinance project/tracked changes of customers' characteristics/sqls/3_tmp_sqq_maru.sql
@@ -1,84 +1,84 @@
--- 测试通过
--- 删除已有tmp_sqq_maru表
+-- Test passed
+-- Delete existing tmp_sqq_maru table
 DROP TABLE IF EXISTS risk_analysis.tmp_sqq_maru;
--- 新建tmp_sqq_maru表
+-- Create a new tmp_sqq_maru table
 CREATE TABLE risk_analysis.tmp_sqq_maru
 COMMENT 'main&rule'
 STORED AS PARQUET
 AS
--- 创建内部表
+-- Create an internal table
 WITH
-main AS (
+Main AS (
 SELECT
-appl_no
+Appl_no
 FROM
-risk_analysis.tmp_sqq_main
+Risk_analysis.tmp_sqq_main
 ),
-rule AS (
+Rule AS (
 SELECT
-appl_no,
+Appl_no,
 GROUP_CONCAT(rule_id) AS rule_id_cat
 FROM
-dsst.gdl_aprvadt_trig_rule
+Dsst.gdl_aprvadt_trig_rule
 WHERE
-dt = '20180609'
--- dt = FROM_UNIXTIME(UNIX_TIMESTAMP()-86400,'yyyyMMdd')
+Dt = '20180609'
+-- dt = FROM_UNIXTIME(UNIX_TIMESTAMP()-86400, 'yyyyMMdd')
 AND
 (
-rule_id like ('HC%')
+Rule_id like ('HC%')
 OR
-rule_id like ('SLBE%')
+Rule_id like ('SLBE%')
 OR
-rule_id like ('SLED%')
+Rule_id like ('SLED%')
 OR
-rule_id like ('Flag%')
+Rule_id like ('Flag%')
 OR
-rule_id like ('SLFD%')
+Rule_id like ('SLFD%')
 OR
-rule_id like ('FDIN%')
+Rule_id like ('FDIN%')
 )
-GROUP BY 
-appl_no
+GROUP BY
+Appl_no
 ),
--- 注意,这是张增量表
-oper AS (
+-- Note that this is a increment table
+Oper AS (
 SELECT
-oper_uniq.app_no,
+Oper_uniq.app_no,
 SUM(oper_uniq.oper_rslt) AS ops2ce
 FROM
 (
 SELECT
-oper_rec.app_no,
-oper_rec.oper_rslt,
-row_number() OVER ( PARTITION BY app_no ORDER BY comp_tm DESC) AS rn
-FROM 
-dsst.fdl_aprvadt_luma_appl_task_oper_rec AS oper_rec
-WHERE
-oper_rslt=9 
-AND 
-comp_tm IS NOT NULL
-) AS oper_uniq
-WHERE 
-oper_uniq.rn=1
-GROUP BY
-oper_uniq.app_no
-)
--- 查询语句块
-SELECT
-main.appl_no,
-rule.rule_id_cat,
-oper.ops2ce
+Oper_rec.app_no,
+Oper_rec.oper_rslt,
+Row_number() OVER ( PARTITION BY app_no ORDER BY comp_tm DESC) AS rn
 FROM
-main
+Dsst.fdl_aprvadt_luma_appl_task_oper_rec AS oper_rec
+WHERE
+Oper_rslt=9
+AND
+Comp_tm IS NOT NULL
+) AS oper_uniq
+WHERE
+Oper_uniq.rn=1
+GROUP BY
+Oper_uniq.app_no
+)
+-- Query block
+SELECT
+Main.appl_no,
+Rule.rule_id_cat,
+Oper.ops2ce
+FROM
+Main
 LEFT JOIN
-rule
+Rule
 ON
-main.appl_no = rule.appl_no
+Main.appl_no = rule.appl_no
 LEFT JOIN
-oper
+Oper
 ON
-main.appl_no = oper.app_no
+Main.appl_no = oper.app_no
 ;
--- SQL结束
--- 刷新
+-- SQL ends
+-- Refresh
 INVALIDATE METADATA risk_analysis.tmp_sqq_maru;
